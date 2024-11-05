6a02504c748800983d32f49bbd81caadc88ae2d2
cocacolaja <mmoniapl@gmail.com>
2024-11-05 19:13:06 +0100

    Add files via upload

---
+++ C#6/LZespolona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia6
+{
+    internal class LZespolona
+    {
+        double re;
+        double im;
+
+        public LZespolona(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public override string ToString()
+        {
+            return $"{re:f2}{(im > 0 ? "+" : "-")}{Math.Abs(im):f2}j";
+        }
+
+        // przeładowanie operatorów
+        //przeciazamy metode dodawania
+        public static LZespolona operator+(LZespolona x, LZespolona y)
+        {
+            return new(x.re + y.re, x.im + y.im);
+        }
+
+        //przeciazamy metode odejmowania
+        public static LZespolona operator -(LZespolona x, LZespolona y)
+        {
+            return new(x.re - y.re, x.im - y.im);
+        }
+
+        // (x.re,x.im)(y.re,y.im) = (x.re*y.re-y.im*x.im, xre*y.im + x.im*y.re)
+        public static LZespolona operator*(LZespolona x, LZespolona y)
+        {
+            return new(x.re * y.re - y.im * x.im, x.re * y.im + x.im * y.re);
+        }
+
+        public static LZespolona operator/(LZespolona x, LZespolona y)
+        {
+            // dodać sprawdzeni czy dzielnik !=0
+            return new(((x.re * y.re) + (x.im * y.im)) / (y.re * y.re + y.im * y.im), ((x.im * y.re) - (x.re * y.im)) / (y.re * y.re + y.im * y.im)); // nie ma ^2 ???
+        }
+
+        public static LZespolona Conj(LZespolona x)
+        {
+            return new(x.re, -x.im);
+        }
+
+        // modul
+        public static double Abs(LZespolona x)
+        {
+            return Math.Sqrt(x.re*x.re + x.im*x.im);
+        }
+
+       
+    }
+}
---
+++ C#6/Program.cs
@@ -0,0 +1,53 @@
+﻿// do domu klasa ułamek, zdefionowac wszystkie operacje podstw., skrócić,
+// przedstawic jako czesc calkowita i ulamek, wymyslic cos jak zadanie 1, wybranie najw z listy albo cos w tym stylu, kazdy inne(posortowac
+// wyslac zadanie domowe
+// dopisać w zadaniu 1 sortowanie ze zdjęcia
+
+namespace Cwiczenia6
+{
+    class Program
+    {
+        static void Test1()
+        {
+            LZespolona z1 = new(5, 6);
+            LZespolona z2 = new(-5, 2);
+
+            Console.WriteLine(z1);
+            Console.WriteLine(z2);
+
+            Console.WriteLine(z1 + z2);
+            Console.WriteLine(z1 - z2);
+            Console.WriteLine(z1 * z2);
+            Console.WriteLine(z1 / z2);
+        }
+
+        static void Zadanie1()
+        {
+            //lista liczb zespolonych gdzie liczby zespolone to l. losowe i posortowć wedlug modulu
+            Random rand = new Random();
+            List<LZespolona> lzespolone = new();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                LZespolona z = new(rand.NextDouble() * 20 - 10 + 1, rand.NextDouble() * 20 - 10 + 1);
+                lzespolone.Add(z);
+            }
+
+            lzespolone.ForEach(lz => Console.WriteLine($"{lz}"));
+            //liczbyZespolone.ForEach
+            
+            lzespolone.Sort((l1,l2)=> LZespolona.Abs(lz1).CompareTo(LZespolona.Abs(lz2))
+
+
+
+        }
+
+        static void Main()
+        {
+            //Test1();
+            Zadanie1();
+            //lista liczb zespolonych gdzie liczby zespolone to l. losowe i posortowć wedlug modulu
+        }
+    }
+}
