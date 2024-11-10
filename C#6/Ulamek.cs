93bceb4dd3c7c5cd7d2fde394b1b44d3a011af22
cocacolaja <mmoniapl@gmail.com>
2024-11-10 23:02:29 +0100

    Create Ulamek.cs

---
+++ C#6/Ulamek.cs
@@ -0,0 +1,4 @@
+﻿// do domu klasa ułamek, zdefionowac wszystkie operacje podstw., skrócić,
+// przedstawic jako czesc calkowita i ulamek, wymyslic cos jak zadanie 1, wybranie najw z listy albo cos w tym stylu, kazdy inne(posortowac
+// wyslac zadanie domowe
+// dopisać w zadaniu 1 sortowanie ze zdjęcia
