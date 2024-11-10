3afde4e80eaf748166a3a0d60af0aae05e228af7
cocacolaja <mmoniapl@gmail.com>
2024-11-11 00:59:35 +0100

    Update Ulamek.cs

--- C#6/Ulamek.cs
+++ C#6/Ulamek.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,95 +8,114 @@ namespace Cwiczenia6
 {
     internal class Ulamek
     {
-        if (mianownik == 0)
-        {
-            throw new ArgumentException("Mianownik nie może być zerem.");
-        }
+
         int licznik;
         int mianownik;
 
+
         public Ulamek(int licznik, int mianownik)
         {
+            if (mianownik == 0) {
+                throw new ArgumentException("Mianownik nie może być zerem.");
+            }
+
             this.licznik = licznik;
             this.mianownik = mianownik;
         }
 
         public override string ToString()
         {
-            if(x.licznik > x.mianownik){
-            return Postac(this);}
-            else{
-            return $"({x.licznik}/{x.mianownik})"};
+            return Postac(this);
         }
 
         // przeładowanie operatorów
         //przeciazamy metode dodawania
-        public static Ulamek operator+(Ulamek x, Ulamek y)
+        public static Ulamek operator +(Ulamek x, Ulamek y)
         {
-            if(x.mianownik ==y.mianownik){
-            return new(x.licznik+y.licznik, x.mianownik);}
-            else if(x.mianownik % y.mianownik ==0){
-            return new(x.licznik+y.licznik*(x.mianownik/y.mianownik, x.mianownik));}
-            else if(y.mianownik % x.mianownik ==0){
-            return new(y.licznik+x.licznik*(y.mianownik/x.mianownik, y.mianownik));}
-            else{
-            return new(x.licznik*y.mianownik+y.licznik*x.mianownik, x.mianownik*y.mianownik);}
+            if (x.mianownik == y.mianownik)
+            {
+                return new(x.licznik + y.licznik, x.mianownik);
+            }
+            else if (x.mianownik % y.mianownik == 0)
+            {
+                return new(x.licznik + y.licznik * (x.mianownik / y.mianownik), x.mianownik);
+            }
+            else if (y.mianownik % x.mianownik == 0)
+            {
+                return new(y.licznik + x.licznik * (y.mianownik / x.mianownik), y.mianownik);
+            }
+            else
+            {
+                return new(x.licznik * y.mianownik + y.licznik * x.mianownik, x.mianownik * y.mianownik);
+            }
         }
 
         //przeciazamy metode odejmowania
         public static Ulamek operator -(Ulamek x, Ulamek y)
         {
-            if(x.mianownik ==y.mianownik){
-            return new(x.licznik-y.licznik, x.mianownik);}
-            else if(x.mianownik % y.mianownik ==0){
-            return new(x.licznik-y.licznik*(x.mianownik/y.mianownik, x.mianownik));}
-            else if(y.mianownik % x.mianownik ==0){
-            return new(y.licznik-x.licznik*(y.mianownik/x.mianownik, y.mianownik));}
-            else{
-            return new(x.licznik*y.mianownik-y.licznik*x.mianownik, x.mianownik*y.mianownik);}
+            if (x.mianownik == y.mianownik)
+            {
+                return new(x.licznik - y.licznik, x.mianownik);
+            }
+            else if (x.mianownik % y.mianownik == 0)
+            {
+                return new(x.licznik - y.licznik * (x.mianownik / y.mianownik), x.mianownik);
+            }
+            else if (y.mianownik % x.mianownik == 0)
+            {
+                return new(y.licznik - x.licznik * (y.mianownik / x.mianownik), y.mianownik);
+            }
+            else
+            {
+                return new(x.licznik * y.mianownik - y.licznik * x.mianownik, x.mianownik * y.mianownik);
+            }
         }
 
         // (x.re,x.im)(y.re,y.im) = (x.re*y.re-y.im*x.im, xre*y.im + x.im*y.re)
-        public static Ulamek operator*(Ulamek x, Ulamek y)
+        public static Ulamek operator *(Ulamek x, Ulamek y)
         {
-            return new(x.licznik*y.licznik, x.mianownik*y.mianownik);
+            return new(x.licznik * y.licznik, x.mianownik * y.mianownik);
         }
 
-        public static Ulamek operator/(Ulamek x, Ulamek y)
+        public static Ulamek operator /(Ulamek x, Ulamek y)
         {
-            return new(x.licznik*y.mianownik, x.mianownk*y.licznik);
+            return new(x.licznik * y.mianownik, x.mianownik * y.licznik);
         }
         private static int NWD(int a, int b)
         {
-          while (b != 0)
-          {
-            int temp = b;
-            b = a % b;
-            a = temp;
-          }
-          return Math.Abs(a); // NWD zawsze dodatni
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a); // NWD zawsze dodatni
         }
 
         public static Ulamek Skracanie(Ulamek x)
         {
-            int nwd = NWD(x.licznik,x.mianownik);
-            return new(x.licznik/NWD, x.mianownik/NWD);
+            int nwd = NWD(x.licznik, x.mianownik);
+            return new(x.licznik /nwd, x.mianownik /nwd);
         }
 
         public static string Postac(Ulamek x)
-        {  
-            int reszta = x.licznik%x.mianownik;
-            int calkowite = (x.licznik - reszta)/x.mianownik;
-            if(x.licznik > x.mianownik){
-            return($"{calkowite}({reszta}/{x.mianownik})");
-            else{
-            return($"{x.licznik}/{x.mianownik}");}
+        {
+            int reszta = x.licznik % x.mianownik;
+            int calkowite = (x.licznik - reszta) / x.mianownik;
+            if (x.licznik > x.mianownik)
+            {
+                return ($"{calkowite}({reszta}/{x.mianownik})");
+            }
+            else
+            {
+                return ($"{x.licznik}/{x.mianownik}");
+            }
         }
-            
+
         public static double Dziesietny(Ulamek x)
         {
-            return (double)x.licznik/x.mianownik;
+            return (double)x.licznik / x.mianownik;
         }
-       
+
     }
 }
